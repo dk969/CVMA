ac0d35b8664d71497ed6b2d2cdbf9fce112d3d38
dk969 <43640825+dk969@users.noreply.github.com>
2020-05-10 21:26:24 +0100

    Validation error

--- app/models/subscribe.js
+++ app/models/subscribe.js
@@ -3,20 +3,10 @@ var Schema = mongoose.Schema;
 var validate = require('mongoose-validator');
 
 
-var emailValidator = [
-    validate({
-        validator: 'isEmail',
-        message: "Is not a valid Email."
-    }),
-    validate({
-        validator: 'isLength',
-        arguments: [3, 25],
-        message: "Email should be between {ARGS[0]} amd {ARGS[1]} characters"
-    })
-];
+
 // The array for email subscriptions
 let Subscribe = new Schema({
-emails: [ { validate: emailValidator
+emails: [ {
 }],
 
 },{
